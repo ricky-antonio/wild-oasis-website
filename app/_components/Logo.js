c53f2e5e7a82d744a894423482cafd8e4d64ba13
Ricardo Monterrsoa <48804047+ricky-antonio@users.noreply.github.com>
2025-04-12 14:54:34 -0700

    build home page

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,14 +1,21 @@
+import Image from "next/image";
 import Link from "next/link";
+import logo from "@/public/logo.png";
 
 function Logo() {
-  return (
-    <Link href="/" className="flex items-center gap-4 z-10">
-      <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
-      <span className="text-xl font-semibold text-primary-100">
-        The Wild Oasis
-      </span>
-    </Link>
-  );
+    return (
+        <Link href="/" className="z-10 flex items-center gap-4">
+            <Image
+                src={logo}
+                height="60"
+                width="60"
+                alt="The Wild Oasis logo"
+            />
+            <span className="text-xl font-semibold text-primary-100">
+                The Wild Oasis
+            </span>
+        </Link>
+    );
 }
 
 export default Logo;
--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ const josefin = Josefin_Sans({
     display: "swap",
 });
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
     title: {
@@ -19,11 +20,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
-                <Logo />
-                <Navigation />
-                <main>{children}</main>
-                <footer>Copyright 2025 The Wild Oasis</footer>
+            <body
+                className={`${josefin.className} flex min-h-screen flex-col bg-primary-950 text-primary-100 antialiased`}
+            >
+                <Header />
+                <div className="flex-1 px-8 py-12">
+                    <main className="mx-auto max-w-7xl">{children}</main>
+                </div>
             </body>
         </html>
     );
