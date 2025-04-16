a017a3395080c85e60e0a1da36f895670ebd523d
Ricardo Monterrsoa <48804047+ricky-antonio@users.noreply.github.com>
2025-04-16 11:01:25 -0700

    create API endpoint

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,13 @@
+import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
+
+export const GET = async (request, { params }) => {
+    const { cabinId } = params;
+    
+    try {
+        const [cabin, bookedDates] = await Promise.all([getCabin(cabinId), getBookedDatesByCabinId(cabinId)]) ;
+        return Response.json({ cabin, bookedDates });
+    } catch {
+        return Response.json({message: "Cabin could not be found"})
+    }
+
+};
