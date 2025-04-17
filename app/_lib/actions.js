2ed6f1ed1e805996e66ee7d1c809a7908fbbcd6c
Ricardo Monterrsoa <48804047+ricky-antonio@users.noreply.github.com>
2025-04-17 13:44:52 -0700

    delete user reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/_lib/auth";
 import { supabase } from "./supabase";
 import { revalidatePath } from "next/cache";
+import { getBookings } from "./data-service";
 
 export const updateProfile = async (formData) => {
     const session = await getServerSession(authOptions);
@@ -25,10 +26,30 @@ export const updateProfile = async (formData) => {
         .update(updateData)
         .eq("id", session.user.guestId);
 
-    revalidatePath("/account/profile")
-
     if (error) {
         console.error(error);
         throw new Error("Guest could not be updated");
     }
+
+    revalidatePath("/account/profile");
+};
+
+export const deleteReservation = async (bookingId) => {
+    const session = await getServerSession(authOptions);
+    if (!session) throw new Error("You must be logged in.");
+
+    const guestBookings = await getBookings(session.user.guestId);
+    const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+    if (!guestBookingIds.includes(bookingId))
+        throw new Error("You do not have access to this booking.");
+
+    const { error } = await supabase
+        .from("bookings")
+        .delete()
+        .eq("id", bookingId);
+
+    if (error) throw new Error("Booking could not be deleted");
+
+    revalidatePath("/account/reservations");
 };
